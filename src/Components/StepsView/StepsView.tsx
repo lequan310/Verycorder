25191425554a086ec41658993e4faa355d015e3f
Phi Thai <thaimanhphi@gmail.com>
2024-05-24 15:33:47 +0700

    add interface for 'api'

--- src/Components/StepsView/StepsView.tsx
+++ src/Components/StepsView/StepsView.tsx
@@ -1,50 +1,32 @@
 import React, { useState } from "react";
 import StepItem from "./StepItem/StepItem";
 import "./StepsView.css";
-import { RecordedEvent } from "../../Types/recordedEvent"
+import { RecordedEvent } from "../../Types/recordedEvent";
 
 const StepsView = () => {
-    const ipcRenderer = (window as any).api;
-    const [eventList, setEventList] = useState([]);
+  const ipcRenderer = window.api;
+  const [eventList, setEventList] = useState([]);
 
-    const addEvent = (event: RecordedEvent) => {
-        setEventList([...eventList, event]);
-    };
+  const addEvent = (event: RecordedEvent) => {
+    setEventList([...eventList, event]);
+  };
 
-    ipcRenderer.on(`add-event`, (event: RecordedEvent) => {
-        addEvent(event);
-    });
+  ipcRenderer.on(`add-event`, (event: RecordedEvent) => {
+    addEvent(event);
+  });
 
-    ipcRenderer.on(`toggle-record`, (recording: boolean) => {
-        if (recording) setEventList([]); // Reset event list when recording starts
-        else ipcRenderer.send('update-test-case', eventList); // Send recorded events to main process when finish recording
-    });
+  ipcRenderer.on(`toggle-record`, (recording: boolean) => {
+    if (recording) setEventList([]); // Reset event list when recording starts
+    else ipcRenderer.send("update-test-case", eventList); // Send recorded events to main process when finish recording
+  });
 
-    return (
-        <div className="stepview__container">
-            {eventList.map((event, index) => {
-                return (
-                    <StepItem key={index} />
-                );
-            })}
-            {/* <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem />
-            <StepItem /> */}
-        </div>
-    );
+  return (
+    <div className="stepview__container">
+      {eventList.map((event, index) => {
+        return <StepItem key={index} />;
+      })}
+    </div>
+  );
 };
 
 export default StepsView;
