23f8a7a458e34b013e5546227b0495c2cbf350ef
Quan Le <leminhquan310@gmail.com>
2024-07-28 20:10:02 +0700

    feat: detect hover with bbox

--- src/Types/bbox.ts
+++ src/Types/bbox.ts
@@ -4,4 +4,8 @@ export class BoundingBox {
     contains(mouseX: number, mouseY: number): boolean {
         return mouseX >= this.x1 && mouseX <= this.x2 && mouseY >= this.y1 && mouseY <= this.y2;
     }
+
+    onEnter(prevMouseX: number, prevMouseY: number, mouseX: number, mouseY: number): boolean {
+        return !this.contains(prevMouseX, prevMouseY) && this.contains(mouseX, mouseY);
+    }
 }
