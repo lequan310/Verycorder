bf5dcc4be311cbb6defbd42d8511dcb9ae167a1d
Quan Le <leminhquan310@gmail.com>
2024-07-28 20:11:17 +0700

    fix: function name entered

--- src/Types/bbox.ts
+++ src/Types/bbox.ts
@@ -5,7 +5,7 @@ export class BoundingBox {
         return mouseX >= this.x1 && mouseX <= this.x2 && mouseY >= this.y1 && mouseY <= this.y2;
     }
 
-    onEnter(prevMouseX: number, prevMouseY: number, mouseX: number, mouseY: number): boolean {
+    entered(prevMouseX: number, prevMouseY: number, mouseX: number, mouseY: number): boolean {
         return !this.contains(prevMouseX, prevMouseY) && this.contains(mouseX, mouseY);
     }
 }
