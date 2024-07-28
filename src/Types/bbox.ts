b2df6a3733fa7609da7f61284123a747a2b3453c
Quan Le <leminhquan310@gmail.com>
2024-07-28 20:06:59 +0700

    feat: create class BoundingBox

---
+++ src/Types/bbox.ts
@@ -0,0 +1,7 @@
+export class BoundingBox {
+    constructor(public x1: number, public x2: number, public y1: number, public y2: number) { }
+
+    contains(mouseX: number, mouseY: number): boolean {
+        return mouseX >= this.x1 && mouseX <= this.x2 && mouseY >= this.y1 && mouseY <= this.y2;
+    }
+}
