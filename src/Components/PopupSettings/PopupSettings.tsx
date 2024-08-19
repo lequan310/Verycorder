976b492875b2af78d4f89e3969df18d2ba23ab81
Phi Thai <thaimanhphi@gmail.com>
2024-08-19 23:31:20 +0700

    feat: add similarity button

--- src/Components/PopupSettings/PopupSettings.tsx
+++ src/Components/PopupSettings/PopupSettings.tsx
@@ -1,4 +1,11 @@
-import { Dispatch, SetStateAction, useContext, useEffect, useRef } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import "./PopupSettings.css";
 import {
   TargetContext,
@@ -17,6 +24,7 @@ const PopupSettings = ({
 }) => {
   const targetContext = useContext(TargetContext);
   const dispatch = useContext(TargetDispatchContext);
+  const [similarity, setSimilarity] = useState(0);
   const ipcRenderer = window.api;
 
   const setTarget = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,7 +34,8 @@ const PopupSettings = ({
     dispatch({ type: "SET_TARGET", payload: newTarget });
   };
 
-  const updateDetectMode = (detectMode: DetectType) => {
+  const updateDetectMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const detectMode = event.target.checked ? DetectMode.AI : DetectMode.DOM;
     popupState(false);
     if (dispatch) {
       ipcRenderer.send(Channel.win.UPDATE_DETECT_MODE, detectMode);
@@ -57,42 +66,50 @@ const PopupSettings = ({
 
   return (
     <div className="popup_wrapper" ref={popupRef}>
-      <div>
-        <p>Test detector: </p>
-        <div className="toggle_wrapper">
-          <div className="toggle-switch">
-            <input
-              className="toggle-input"
-              id="toggle"
-              type="checkbox"
-              checked={targetContext.target === TargetEnum["x-path"]}
-              onChange={setTarget}
-            />
-            <label className="toggle-label" htmlFor="toggle"></label>
-          </div>
-          <p>X-path</p>
-        </div>
+      <div className="popup_button">
+        <label htmlFor="xpath">Use X-Path</label>
+
+        <input
+          id="xpath"
+          type="checkbox"
+          checked={targetContext.target === TargetEnum["x-path"]}
+          onChange={setTarget}
+        />
+        <div className="divider fixed_bottom"></div>
       </div>
-      <div>
-        <p>Target: </p>
-        <select
-          name="target"
-          id="target"
-          value={targetContext.detectMode}
-          onChange={(e) => {
-            updateDetectMode(e.target.value as DetectType);
-          }}
+      <div className="popup_button">
+        <label htmlFor="ai">Detect with AI ✨</label>
+
+        <input
+          id="ai"
+          type="checkbox"
+          checked={targetContext.detectMode === DetectMode.AI}
+          onChange={updateDetectMode}
           disabled={
             targetContext.recordState ||
             targetContext.replayState ||
             !targetContext.editState
           }
-        >
-          <option value={DetectMode.DOM}>{DetectMode.DOM}</option>
-          <option value={DetectMode.AI}>{DetectMode.AI}</option>
-        </select>
+        />
+        <div className="divider fixed_bottom"></div>
       </div>
-      {/* <div className="close_wrapper"></div> */}
+      {targetContext.detectMode === DetectMode.AI ? (
+        <div className="popup_button">
+          <div className="popup_button_col">
+            <label htmlFor="ai">Similarity</label>
+            <p>The higher the similarity, the higher the accuracy</p>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value={similarity}
+              onChange={(e) => setSimilarity(parseInt(e.target.value))}
+              className="slider"
+              id="myRange"
+            ></input>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
