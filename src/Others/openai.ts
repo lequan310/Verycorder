35ea375c6649571abfeeeb9fb043894194bd7dd5
Quan Le <leminhquan310@gmail.com>
2024-09-01 01:11:21 +0700

    feat: optimize openai stuff

--- src/Others/openai.ts
+++ src/Others/openai.ts
@@ -13,40 +13,37 @@ const Result = z.object({
 dotenv.config();
 
 const openai = new OpenAI();
-const caption_model = "gpt-4-turbo";
+const caption_model = "gpt-4o-2024-08-06";
 const locate_model = "gpt-4o-2024-08-06";
 const embed_model = "text-embedding-3-large";
 
-const CAPTION_PROPMT = `You are an expert at captioning web element. You are given an image of a web element.
-Generate a single precise locator for the web element based on visual description.
-Visual Description should contains all details available from the web element.
-Example attributes of details include but not limited to text, background color, shape, icon, etc.
-If the visual description includes the icon, you can describe the icon with its name if you know. Example: icon="hamburger" or icon="youtube". If you don't know the icon name, describe the icon.
-If the element includes or is an icon, you can assume it is a button.
+const CAPTION_PROPMT = `You are an expert at captioning web element. You are given an image of a web element. \
+Generate a single precise locator for the web element based on visual description. \
+Visual Description should contains all details available from the web element. \
+Example attributes of details include but not limited to text, background color, shape, icon, etc. \
+If the visual description includes an icon, you can describe the icon with its name if you know. Example: icon="hamburger" or icon="youtube". If you don't know the icon name, describe the icon. \
+If the element contains an icon, you can assume it is button.
 
-Locator format: [<Element> with description based on visual]. Element can only be button, text, input field, or image.
-Element description example: [button with text="Sign in", background_color="Light Purple", shape="rectangle"]
-For image elements, if you recognize the image, say the name of the image and the visual description. Locator format for image elements: [image of <image name> with description based on visual]
-If you don't recognize the image, describe the image. Locator format for image elements: [image description based on visual]
-Answer should be within 50 words.`;
+Locator format: <[Element] with description based on visual>. Element can only be button, text, input field, or image. \
+For image elements, if you recognize the image, say the name of the image and describe the visual description. \
+If you don't recognize the image, describe the visual description. \
+Locator example: <button with content="Sign in", background_color="Dark Blue", shape="rectangle">
+Locator should be within 100 words.`;
 
-const LOCATE_PROMPT = `You are an expert at locating web element based on visual description. 
-You are provided a screenshot of a web page with bounding boxes around the web elements and the index number on the TOP LEFT of the corresponding bounding box.
-You are also provided with a visual description of a web element.
-Numbers may be hard to see, but they should have the same color with the corresponding bounding box.
+const LOCATE_PROMPT = `You are an expert at locating web element based on visual description. \
+You are provided with a screenshot of a web page with the bounding boxes annotating the web elements and the index number of the corresponding bounding box. \
+Index number has the same color with the corresponding bounding box border color and are positioned on the TOP LEFT CORNER of the corresponding bounding box. \
+You are also provided with a visual description of a web element that you need to identify. \
 
 You should do the following steps to improve your accuracy after receving the image and the description:
 1. Go through each bounding box from left to right, top to bottom.
-2. For each web element within the bounding box, identify whether they match the visual description.
-For an element to match the visual description, it must contain all the features provided in the visual description.
-For example, if the element locator is [button with text="Sign in", background_color=#0b0b0b, shape="rectangle"], the element must have the text "Sign in", background color #0b0b0b, and shape "rectangle".
+2. For each web element within the bounding box, identify whether they match the visual description. \
+For an element to match the visual description, it must contain all the features provided in the visual description. \
+For example, if the element locator is <button with content="Sign in", background_color="Dark Blue", shape="rectangle">, the element must have content text "Sign in", background color "Dark Blue", and shape "rectangle".
 3. If the web element matches the visual description, provide the index number of the bounding box and stop the process.
 4. After going through all the web elements, if no element matches the visual description, answer -1.
 
-Give a single number, which is the index of the bounding box, as the answer.
-
-Begin!
-`;
+Answer format: a single number, which is the index of the bounding box. If no element matches the visual description, answer -1.`;
 
 let similarityValue = 0.8;
 
@@ -76,18 +73,21 @@ async function getSimilarity(
     locator: string,
     newLocator: string
 ): Promise<number> {
-    const embeddingObject = await openai.embeddings.create({
-        model: embed_model,
-        input: locator.replace("[", "").replace("]", ""),
-        encoding_format: "float",
-    });
+    const [embeddingObject, newEmbeddingObject] = await Promise.all([
+        openai.embeddings.create({
+            model: embed_model,
+            input: locator.replace("<", "").replace(">", ""),
+            encoding_format: "float",
+        }),
+        openai.embeddings.create({
+            model: embed_model,
+            input: newLocator.replace("<", "").replace(">", ""),
+            encoding_format: "float",
+        }),
+    ]);
 
-    const newEmbeddingObject = await openai.embeddings.create({
-        model: embed_model,
-        input: newLocator.replace("[", "").replace("]", ""),
-        encoding_format: "float",
-    });
-
+    console.log(locator);
+    console.log(newLocator);
     const embedding = embeddingObject.data[0].embedding;
     const newEmbedding = newEmbeddingObject.data[0].embedding;
     const similarity = cosine_similarity(embedding, newEmbedding);
@@ -161,7 +161,7 @@ export async function getReplayBoundingBox(drawnBuffer: Buffer, bboxes: Bounding
                     content: [
                         {
                             type: "text",
-                            text: `Find the index of the web element with the same visual properties as following: ${locator}. Returns -1 if the element with similar visual properties is not found.`,
+                            text: `Find the index of the web element with the same visual properties as following: ${locator}`,
                         },
                         {
                             type: "image_url",
