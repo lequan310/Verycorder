debc74e46555d60edfdd314652a13f09fde1ba55
bentrieu <s3878466@rmit.edu.vn>
2024-09-01 20:12:25 +0700

    [ben] save and load

--- src/Types/testCase.ts
+++ src/Types/testCase.ts
@@ -4,11 +4,9 @@ import { RecordedEvent } from "./recordedEvent"
 export interface TestCase {
     url: string
     events: RecordedEvent[]
-    size: { width: number, height: number }
 }
 
 export interface CanvasTestCase {
     url: string
     events: CanvasEvent[]
-    size: { width: number, height: number }
 }
